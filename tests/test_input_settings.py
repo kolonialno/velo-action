f13d56641f73b8663eb2597190b5cf7d52e2ddca
Trond Hindenes <trond.hindenes@oda.com>
2021-08-06 14:24:30 +0200

    Mark it

--- tests/test_input_settings.py
+++ tests/test_input_settings.py
@@ -1,9 +1,14 @@
 import pytest
 from velo_action.settings import Settings
 from pydantic import ValidationError
+import os
+
+HAS_GITHUB_WORKSPACE = True if os.getenv("GITHUB_WORKSPACE") else False
 
 
+@pytest.mark.skipif(HAS_GITHUB_WORKSPACE, reason="GITHUB_WORKSPACE is set, skipping this")
 def test_assume_fail_arg_parser_noworkspace(monkeypatch):
+    """Will be skipped when running in GH Action, since GITHUB_WORKSPACE is set by the environment"""
     with pytest.raises(ValidationError):
         s = Settings()
 
